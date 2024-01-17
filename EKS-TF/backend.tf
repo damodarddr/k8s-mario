9bef469edf5fc79c679832032404b01d0ec05d02
damodarddr <152080280+damodarddr@users.noreply.github.com>
2024-01-18 00:55:13 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbook-cicd-bucket"
-    # other backend configurations...
+    bucket         = "mrcloudbook-cicd-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "mrcloudbook-dynamo-db-table"
   }
 }
