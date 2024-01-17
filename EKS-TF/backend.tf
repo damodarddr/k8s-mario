927f8ad202ce243099dd2a68df08e09db65cdfc3
damodarddr <152080280+damodarddr@users.noreply.github.com>
2024-01-17 15:44:24 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mrcloudbook-cicd-bucket" # Replace with your actual S3 bucket name
+    bucket = "mrcloudbook-cicd-bucket1" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
